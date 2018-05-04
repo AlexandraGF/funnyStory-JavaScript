1e2861067ff3b18a955934a9b4f8ed472d507e69
Alexy <alexandra.fina@yahoo.com>
2018-05-04 13:34:26 +0100

    Test story cannot be revealed until all options selected

--- spec/funnyStorySpec.js
+++ spec/funnyStorySpec.js
@@ -24,12 +24,21 @@ describe('FunnyStory', function (){
   });
 
   describe('Another option ', function(){
-  it('should replace the old option on the same line', function() {
-    story = new FunnyStory();
-    story.checkItem(0, "PIE");
-    story.checkItem(0, "PASTA");
-    expect(story.array[0]).toEqual("PASTA");
+    it('should replace the old option on the same line', function() {
+      story = new FunnyStory();
+      story.checkItem(0, "PIE");
+      story.checkItem(0, "PASTA");
+      expect(story.array[0]).toEqual("PASTA");
+    });
   });
-});
+
+  describe('Story ', function(){
+    it('must have all options chosen before being revealed', function() {
+      story = new FunnyStory();
+      story.array = [ "SPICY", "RESTAURANT", "HUGE", "FLUFFY TOYS", "FISH", "EVENTUALLY", "ORANGE JUICE", "FRYING", "EYES", "CRY", "WORK", "LAUGHING"];
+      expect(story.checkMissingInfo("Alex", "Dalam")).toEqual("not Revealed");
+    });
+
+  });
 
 });
--- src/funnyStory.js
+++ src/funnyStory.js
@@ -15,8 +15,30 @@ FunnyStory.prototype.checkItem = function (arrayIndex, itemName) {
   }
 };
 
+FunnyStory.prototype.checkMissingInfo = function (name1, name2) {
+  if (this.array.length == 13 && name1.length > 0 && name2.length > 0) {
+  this._getNames(name1, name2);
+  this.revealStory();
+  return "yes";
+  console.log(this.revealStory());
+  }
+  else if (this.array.length != 13 ) {
+    alert("Please finish choosing all your options before revealing your funny story!");
+    return "not Revealed";
+  }
+  else if (name1.length == 0 || name2.length == 0) {
+    alert("Please write two names!");
+    return "not Revealed";
+  }
+};
+
 // private methods
 
 FunnyStory.prototype._getWord = function (word) {
   this.array.push(word);
+};
+
+FunnyStory.prototype._getNames = function (name1, name2) {
+  this.name1 = name1.toUpperCase();
+  this.name2 = name2.toUpperCase();
 };
