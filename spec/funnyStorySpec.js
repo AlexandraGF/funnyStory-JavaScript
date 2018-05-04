5cf89c1b840eb5df647c91920943d1f596910d9b
Alexy <alexandra.fina@yahoo.com>
2018-05-04 13:30:24 +0100

    Test User can select more than one option

--- spec/funnyStorySpec.js
+++ spec/funnyStorySpec.js
@@ -6,5 +6,14 @@ describe('FunnyStory', function (){
       story.checkItem(0, "PASTA")
       expect(story.array[0]).toEqual("PASTA");
     });
+
+    it('select more than one option', function() {
+      story = new FunnyStory();
+      story.checkItem(0, "PASTA")
+      story.checkItem(1, "SPICY")
+      story.checkItem(2, "RESTAURANT")
+      expect(story.array.length).toEqual(3);
+    });
+    
   });
 });
